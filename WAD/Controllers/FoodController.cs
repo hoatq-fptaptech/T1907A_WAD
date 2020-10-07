93c5289f4345b2bea92de0bf7e57033cc63874a9
hoatq-fptaptech <65893435+hoatq-fptaptech@users.noreply.github.com>
2020-10-07 15:01:24 +0700

    up code

---
+++ WAD/Controllers/FoodController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WAD.Models;
+
+namespace WAD.Controllers
+{
+    public class FoodController : Controller
+    {
+        // GET: Food
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string About()
+        {
+            return "about page";
+        }
+
+        public string Contact()
+        {
+            return "contact page";
+        }
+    }
+}
---
+++ WAD/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAD.Models
+{
+    public class Student
+    {
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
